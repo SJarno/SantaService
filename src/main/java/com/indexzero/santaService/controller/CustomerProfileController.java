d7ec214ffff844549de6ab2d0e64f1db36896ace
SJarno <jarno.saastamoinen@hotmail.com>
2021-12-16 18:28:22 +0200

    refactor customer profile

--- src/main/java/com/indexzero/santaService/controller/CustomerProfileController.java
+++ src/main/java/com/indexzero/santaService/controller/CustomerProfileController.java
@@ -12,6 +12,8 @@ import com.indexzero.santaService.repositories.CustomerProfileRepository;
 import com.indexzero.santaService.repositories.OrderRepository;
 import com.indexzero.santaService.services.CustomerProfileService;
 import com.indexzero.santaService.services.OrderService;
+import com.indexzero.santaService.services.RedirectService;
+import com.indexzero.santaService.services.SecurityContextService;
 import com.indexzero.santaService.services.UserAccountService;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,24 +27,28 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class CustomerProfileController {
 
     @Autowired
-    private UserAccountService userAccountService;
+    private CustomerProfileService customerProfileService;
 
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private RedirectService redirectService;
+
+    @Autowired
+    private SecurityContextService securityContextService;
 
     @GetMapping("/customer-profile")
     public String customerProfileView(Model model) {
-        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth.isAuthenticated()) {
-
+        if (securityContextService.getAuthenticatedUser().isAuthenticated()) {
             /* Get useraccount */
-            Optional<UserAccount> userAccount = userAccountService.findUserAccountByUsername(auth.getName());
+            Optional<UserAccount> userAccount = securityContextService.getAuthenticatedUserAccount();
             if (userAccount.isPresent()) {
                 model.addAttribute("basicInfo", userAccount.get());
                 model.addAttribute("profileInfo", userAccount.get().getCustomerProfile());
@@ -52,33 +58,38 @@ public class CustomerProfileController {
 
         return "redirect:login-page";
     }
+
     /* Update profile-info */
-    @Transactional
     @PostMapping("/update/customer-profile")
     public String updateCustomerProfile(
-        @RequestParam String profileName,
-        @RequestParam String email,
-        @RequestParam String deliveryAddress,
-        @RequestParam String postalCode
-        ) {
-        Optional<UserAccount> userAccount = userAccountService
-                .findUserAccountByUsername(getAuthenticatedUser().getName());
-        
-        System.out.println(profileName);
-        if (userAccount.isPresent()) {
-            userAccount.get().getCustomerProfile().setCustomerProfileName(profileName);
-            userAccount.get().getCustomerProfile().setEmail(email);
-            userAccount.get().getCustomerProfile().setDeliveryAddress(deliveryAddress);
-            userAccount.get().getCustomerProfile().setPostalCode(postalCode);
+            @RequestParam String profileName,
+            @RequestParam String email,
+            @RequestParam String deliveryAddress,
+            @RequestParam String postalCode,
+            RedirectAttributes redirectAttributes) {
+
+        boolean success = false;
+        String errorMessage = "";
+        try {
+            success = customerProfileService.updateCustomerProfile(
+                    profileName,
+                    email,
+                    deliveryAddress,
+                    postalCode);
+        } catch (Exception e) {
+            errorMessage = e.getMessage();
+
         }
-        return redirectByUserRole();
+        return redirectService.redirectOnSuccess(success, redirectAttributes, "Päivitetty", errorMessage);
     }
+
     /* Create new order */
     @ResponseBody
     @PostMapping("/customer/{id}/create-order")
     public Order createOrder(@PathVariable Long id) {
         return orderService.createOrder(id);
     }
+
     /* Get orders made by customer: */
     @ResponseBody
     @GetMapping("/customer/orders")
@@ -86,25 +97,19 @@ public class CustomerProfileController {
 
         return orderService.getOrdersByCustomerProfile();
     }
+
     /* Delete order */
     @ResponseBody
     @DeleteMapping("/orders/{id}/delete")
     public Order deleteOrderById(@PathVariable Long id) {
         return orderService.deleteOrder(id);
     }
-    private Authentication getAuthenticatedUser() {
-        return SecurityContextHolder.getContext().getAuthentication();
 
-    }
-    private String redirectByUserRole() {
-        Optional<UserAccount> userAccount = userAccountService
-                .findUserAccountByUsername(getAuthenticatedUser().getName());
-        if (userAccount.isPresent()) {
-            String userRole = userAccount.get().getUserRole();
-            return userRole.equals("ROLE_SANTA") ? "redirect:/santa-profile"
-                    : userRole.equals("ROLE_CUSTOMER") ? "redirect:/customer-profile" : "redirect:/";
-        }
-        return "redirect:/custom-logout";
-    }
+    /*
+     * private Authentication getAuthenticatedUser() {
+     * return SecurityContextHolder.getContext().getAuthentication();
+     * 
+     * }
+     */
 
 }
--- src/main/java/com/indexzero/santaService/services/CustomerProfileService.java
+++ src/main/java/com/indexzero/santaService/services/CustomerProfileService.java
@@ -1,5 +1,10 @@
 package com.indexzero.santaService.services;
 
+import java.util.Optional;
+
+import javax.transaction.Transactional;
+
+import com.indexzero.santaService.model.UserAccount;
 import com.indexzero.santaService.repositories.CustomerProfileRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,9 +13,32 @@ import org.springframework.stereotype.Service;
 @Service
 public class CustomerProfileService {
 
-    @Autowired
-    private CustomerProfileRepository customerProfileRepository;
 
-    
+    @Autowired
+    private SecurityContextService securityContextService;
+
+    @Transactional
+    public boolean updateCustomerProfile(
+        String profileName,
+        String email,
+        String deliveryAddress,
+        String postalCode
+    ) {
+        if (profileName.length() < 3 || profileName.isBlank()) {
+            throw new IllegalArgumentException("Profiilinimi on liian lyhyt");
+        }
+        if (deliveryAddress.isBlank()) {
+            throw new IllegalArgumentException("Toimitusosoite ei saa olla tyhjä");
+        }
+        Optional<UserAccount> userAccount = securityContextService.getAuthenticatedUserAccount();
+        if (userAccount.isPresent()) {
+            userAccount.get().getCustomerProfile().setCustomerProfileName(profileName);
+            userAccount.get().getCustomerProfile().setEmail(email);
+            userAccount.get().getCustomerProfile().setDeliveryAddress(deliveryAddress);
+            userAccount.get().getCustomerProfile().setPostalCode(postalCode);
+            return true;
+        }
+        return false;
+    }
     
 }
